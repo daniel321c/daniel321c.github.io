c01e4e3fa11b68d75dd527fcf62e2946340a4019
daniel321c <321dchen@gmail.com>
2018-06-22 23:19:56 -0400

    all

---
+++ js/demo.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('#lead').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+});
--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,11 @@
-/*!
-    Title: Dev Portfolio Template
-    Version: 1.2.1
-    Last Change: 08/27/2017
-    Author: Ryan Fitzgerald
-    Repo: https://github.com/RyanFitzgerald/devportfolio-template
-    Issues: https://github.com/RyanFitzgerald/devportfolio-template/issues
-
-    Description: This file contains all the scripts associated with the single-page
-    portfolio website.
-*/
-
 (function($) {
-
+    $(document).ready(function() {
+        $('#lead').particleground({
+          dotColor: '#5cbdaa',
+          lineColor: '#5cbdaa'
+        });
+      
+      });
     // Remove no-js class
     $('html').removeClass('no-js');
 
