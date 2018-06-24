ec633a9e2992dadcc5b2767400815084e612255a
daniel321c <321dchen@gmail.com>
2018-06-24 19:59:11 -0400

    release 1

--- js/demo.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('#lead').particleground({
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
-  });
-  $('.intro').css({
-    'margin-top': -($('.intro').height() / 2)
-  });
-});
